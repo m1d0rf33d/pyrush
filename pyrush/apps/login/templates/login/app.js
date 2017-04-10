0a9ab462343b8376e087f117cae80c28d2d3fe52
aomine daiki <evillagomesa@yondu.com>
2017-04-10 09:31:26 +0800

    working reactjs router with django

---
+++ pyrush/apps/login/templates/login/app.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Match from 'react-router';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+let About = () => {
+    return <div> About </div>;
+}
+
+let Home = () => {
+    return <div> Homes </div>;
+}
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+            <div>
+            <Switch>
+                <Route path="/pyrush/login/about/" component={About} />
+                <Route path="/pyrush/login/" exact component={Home} />
+            </Switch>
+            </div>
+            </Router>
+            ); 
+    }
+}
+
+export default App;
